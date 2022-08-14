754bc996b8bc9ea87add37d6ebf21c9045bf6522
AnnaShliapkina <deer1anna@gmail.com>
2022-08-14 18:31:45 +0300

    add styles

---
+++ src/components/app.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Main = styled.div`
+  height: 100vh;
+  color: black;
+  font-weight: 700;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+`;
